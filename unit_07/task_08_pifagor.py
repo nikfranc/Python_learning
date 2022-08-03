682307073e72dd56a4ba730fefe629e4f7b7c896
nikfranc <francevnikolaj774@gmail.com>
2022-08-03 18:40:18 +0300

    updated task 8 in unit 7

--- unit_07/task_08_pifagor.py
+++ unit_07/task_08_pifagor.py
@@ -6,7 +6,7 @@ for m in range(1, num + 1):
             y = 2 * m * n
             z = m ** 2 + n ** 2
             no_divider = True
-            for i in range(2, min(x, y, z) + 1):
+            for i in range(2, min(x, y, z) // 2 + 1):
                 if x % i == 0 and y % i == 0 and z % i == 0:
                     no_divider = False
             if no_divider:
