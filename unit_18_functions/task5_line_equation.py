b5821d27dfc112f7cb8297fe4376c573beac0d1b
nikfranc <francevnikolaj774@gmail.com>
2022-11-24 20:58:56 +0300

    added unit_18_functions

--- unit_18_functions/task5_line_equation.py
+++ unit_18_functions/task5_line_equation.py
@@ -1,8 +1,6 @@
 def equation(m, n):
     m = list(map(float, m.split(';')))
     n = list(map(float, n.split(';')))
-    print(m)
-    print(n)
     k = -(n[1] - m[1]) / (m[0] - n[1])
     b = -(n[0] * m[1] - m[0] * n[1]) / (m[0] - n[0])
     if k != 0:
